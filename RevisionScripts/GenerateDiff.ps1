970fa7b2b142df35e37b07739887f96071b848d7
Brandon-Johns <69143173+Brandon-Johns@users.noreply.github.com>
2023-11-23 13:02:57 +1100

    Add template to respond to examiner comments

---
+++ RevisionScripts/GenerateDiff.ps1
@@ -0,0 +1,164 @@
+<#
+Written By:			Brandon Johns
+Version Created:	2023-11-20
+Last Edited:		2023-11-22
+
+********************************************************************************
+Purpose
+****************************************
+This script runs the latexdiff utility, and does some extra post-processing
+
+latexdiff generates tex file which can be built into a tracked changes pdf
+	Read more about latexdiff here
+	https://www.overleaf.com/learn/latex/Articles/Using_Latexdiff_For_Marking_Changes_To_Tex_Documents
+
+This script is only for documents using the NUMERIC citation type
+	The issue with direct use of latexdiff is that the citation numbers change
+		without consideration of if the cite is inside of a \DIFdel{} or \DIFadd{}.
+		Therefore, the resultant citation numbers don't match any document.
+	This script post-processes the diff file to make
+		\DIFdel{\cite{...}}: uses the numbers of old version
+		\DIFadd{\cite{...}}: uses the numbers of new version
+		\cite{...}:          uses the numbers of new version
+
+This script also generates a .nocite file, which holds the citation order of the new version
+	This is for use to \input{} into the top of your author response to reviewers comments.
+	Hence, the response will use these same citation numbers as the diff file
+
+
+********************************************************************************
+Instructions
+****************************************
+Starting from the version of your thesis, as it was submitted for examination
+
+Install latexdiff
+
+Save old version (DO THIS ONLY ONCE)
+1) Compile Thesis.tex
+2) Run RevisionScripts\GenerateFlattened.ps1
+
+Generate dif (REPEAT AS YOU MAKE CHANGES)
+1) Revise the thesis as required
+2) Compile Thesis.tex
+3) Run RevisionScripts\GenerateDiff.ps1
+4) Write a review response using ReviewResponse.cls
+
+
+********************************************************************************
+Notes
+****************************************
+$fileOld.tex and $fileOld.bibkeys should be in the same directory
+$fileOld.tex should be self-contained (no \input{} commands)
+$fileOld.bibkeys should hold all citation keys in the order that the appear in the bibliography
+	It should be formatted as follows
+```
+% Comments prefixed by a percent are permitted
+example2
+example1
+example3
+example4
+```
+
+#>
+#********************************************************************************
+# CONFIG
+#****************************************
+# Files to compare
+$fileOld = 'Revisions\Thesis_flat'
+$fileNew = 'Thesis'
+
+# Filename for output
+$fileDiff = 'Thesis_diff'
+
+$DiffOptions = @()
+$DiffOptions += '--flatten'
+$DiffOptions += '--preamble=RevisionScripts\DiffPreamble.tex'
+#$DiffOptions += '--graphics-markup=none'
+
+#********************************************************************************
+# AUTOMATED
+#****************************************
+Push-Location $PSScriptRoot\..
+If(-not (Test-Path "${fileOld}.tex"))     { throw "Can't find old file" }
+If(-not (Test-Path "${fileOld}.bibkeys")) { throw "Can't find bibkeys file" }
+If(-not (Test-Path "${fileNew}.tex"))     { throw "Can't find new file" }
+If(-not (Test-Path "${fileNew}.bbl"))     { throw "Can't find bbl file. Compile ${fileNew}.tex, then try again" }
+
+# Generate diff file
+latexdiff $DiffOptions "${fileOld}.tex" "${fileNew}.tex" | Out-File "${fileDiff}.tex" -Encoding utf8
+$diffFileContent = Get-Content -Path "${fileDiff}.tex" -Raw
+
+#********************************************************************************
+# AUTOMATED - Replace all \DIFdel{\cite{...}} with citation numbers according to 
+#****************************************
+# Create associative array (hash table) of key to citation number
+$citedKeys = [ordered]@{}
+$citeNumber = 1
+foreach ($line in Get-Content -Path "${fileOld}.bibkeys") {
+	if ($line -match '^(?!%)(?<citeKey>.+?)$') {
+		$citedKeys[$Matches.citeKey] = $citeNumber;
+		$citeNumber += 1;
+	}
+}
+
+# Search diff file for a '\cite{}' inside of a '\DIFdel{}'
+# Case sensitive match
+$reCite = '\\DIFdel\{[^\}]*?' + '(?<citeCommand>\\cite\{(?<citeKeys>.+?)\})'
+while($diffFileContent -cmatch $reCite) {
+	# Split into array of individual keys
+	$keysInCite = $Matches.citeKeys -split ','
+
+	# Exchange keys for citation numbers and sort numerically
+	$citeNumbersInCite = @()
+	foreach ($key in $keysInCite) {
+		$citeNumbersInCite += $citedKeys[$key];
+	}
+	$citeNumbersInCite = $citeNumbersInCite | Sort-Object {[int]$_}
+
+	# String to write out
+	$FormattedCite = '[' + ($citeNumbersInCite -join ',') + ']'
+
+	# Replace the cite command with the formatted cite
+	$diffFileContent = $diffFileContent.Replace($Matches.citeCommand, $FormattedCite)
+
+	Write-Host $Matches.citeKeys
+	Write-Host $FormattedCite
+	Write-Host ''
+}
+
+# Perform write
+$diffFileContent | Set-Content -Path "${fileDiff}.tex" -Encoding utf8
+
+
+#********************************************************************************
+# AUTOMATED - generate nocite
+#****************************************
+$bibItems = @(@"
+% AUTO-GENERATED FILE
+% This file contains the \bibitem{} or \entry{} content from the .bbl file from compiling ${fileNew}.tex
+% The order of the keys therefore matches the citation numbers
+"@)
+$reBib = '^\s*\\(bibitem|entry)\{(?<bibItem>.+?)\}'
+foreach  ($line in Get-Content -Path "${fileNew}.bbl") {
+	if ($line -cmatch $reBib) {
+		$bibItems += '\nocite{'+$Matches.bibItem+'}'
+	}
+}
+
+$bibItems | Out-File -LiteralPath "${fileNew}.nocite" -Encoding utf8
+
+
+#********************************************************************************
+# AUTOMATED - Build and clean up
+#****************************************
+# Build pdf
+#latexmk -interaction=nonstopmode -pdf "${fileDiff}.tex"
+
+# Clean up
+#Remove-Item -LiteralPath "${fileDiff}.tex"
+
+# Open pdf with chrome
+#Start-Process chrome -ArgumentList (Get-ChildItem "${fileDiff}.pdf").FullName
+
+Pop-Location
+
--- readme.md
+++ readme.md
@@ -1,16 +1,18 @@
 # Monash University Thesis Template
-This template is based on the version provided on the ENG Graduate Research Moodle (Downloaded in 2023).
-
 Compliant with the Monash University requirements.
-
-The complied PDF looks like [this](./Thesis.pdf).
 
 Good for both
 - Traditional thesis
 - Thesis including published works.
 
+The complied PDF looks like [this](./Thesis.pdf).
+
+Also included, is a template to respond to examiner comments.
+
 
 ## My Changes
+This template is based on the version provided on the ENG Graduate Research Moodle (Downloaded in 2023).
+
 I completely reworked the Thesis class and template.
 
 Key changes
@@ -24,6 +26,51 @@ Key changes
 - Made the output PDF/A-2u compliant (well, somewhat compliant: figures and maths are like ugh)
 - Added ability to restore hyperlinks within the embedded pdfs. Not fully automatic- the user has to help it along
 - Completely redid the example scaffolding and example content
+
+
+## Instructions
+### Getting started
+1. Open this project in a LaTeX editor (e.g. import into overleaf)
+2. Ensure that the main document is set to `Thesis.tex`
+3. Test that it compiles
+4. To start editing, open `Thesis.tex` and follow the instructions
+
+
+### Set thesis type
+To set for traditional thesis, `Thesis.tex` should use
+```latex
+%\input{Preamble/Declaration_publications.tex} %% Thesis by Published Works Only
+\input{Preamble/Declaration.tex} %% Traditional Thesis Only
+```
+
+To set for thesis including published works, `Thesis.tex` should use
+```latex
+\input{Preamble/Declaration_publications.tex} %% Thesis by Published Works Only
+%\input{Preamble/Declaration.tex} %% Traditional Thesis Only
+```
+
+This is the only difference between the variants, other than your inclusion of the published work itself.
+
+Examples of how to include your published work are `Chapters\Chapter3.tex` and `Chapters\Chapter4.tex`.
+
+
+### Responding to examiner comments
+The provided template for responding to examiner comments requires running the latexdiff utility and powershell scripts.
+- This can not be done through overleaf.
+- You will need to install latex and latexdiff on your computer.
+- Non-windows users will need to install the cross-platform [PowerShell Core](https://github.com/PowerShell/PowerShell).
+
+Starting from the version of your thesis, as it was submitted for examination
+1. Compile `Thesis.tex`
+2. Run `RevisionScripts\GenerateFlattened.ps1`
+3. Edit your thesis as required to address all examiner comments
+4. Compile `Thesis.tex`
+5. Run `RevisionScripts\GenerateDiff.ps1`
+6. Write `response1.tex`
+    - Quotes of the text with tracked-changes markup can be copied from the generated `Thesis_diff.tex` into the response
+7. Compile `response1.tex`
+
+Return to step 3 as required, but be sure to follow through all subsequent steps to 3-7 to regenerate everything.
 
 
 ## Source Code
